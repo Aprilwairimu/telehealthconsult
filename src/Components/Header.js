9f07f7571e54cecef3d65b3389df918660c43674
Aprilwairimu <100186637+Aprilwairimu@users.noreply.github.com>
2024-07-17 13:59:31 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,7 @@ import { FaUserCircle } from 'react-icons/fa';
 // import Sidebar from './Sidebar/Sidebar';
 import './Header.css';
 import { Avatar } from '@mui/material';
+import { FaCaretDown } from 'react-icons/fa';
 
 const Header = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -23,14 +24,15 @@ const Header = () => {
                 {/* <a href="/login">Log In</a> */}
                 <a href="#support">Support</a>
                 <div alt="Profile Picture" src={profilePicture} onClick={toggleDropdown}>
-                    <FaUserCircle size={30} />
+                    <FaCaretDown size={30} />
                     {dropdownOpen && (
                         <div className="dropdown-menu">
                             <a href="/profile">Profile</a>
                             <a href="/dashboard">Dashboard</a>
                             <a href="/meetings">Bookings & Meetings</a>
-                            {/* <a href="/meetings">Meetings</a> */}
                             <a href="/notifications">Notifications</a>
+                            <a href="/privatepolicy">Private Policy</a>
+                            <a href="/termservice">Terms Service</a>
                         </div>
                     )}
                 </div>
